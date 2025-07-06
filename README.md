1582a3cf6cd2e9cd16dbb27b9b5276ae1f54ad41
Clint Zold <214383878+clintzold@users.noreply.github.com>
2025-07-06 12:44:35 -0600

    Move Calculator files to proper directory
    Accidentally built Calculator in wrong repo folder
    Moved basic work to proper folder
    
    SO FAR:
        Functions for add, subtract, multiply,divide.
        Function that rounds decimal numbers to 2
        points
        Basic HTML skeleton with appropriate DIVs for
        buttons, display, etc...
        Beginning CSS stylings of calculator HTML

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # calculator
+This is a calculator web-app for The Odin Project - Foundations course.
+
+The calculator will evaluate number pairs by a selected operator. Originally,
+I planned to evaluate an array of value or a string, but for security reasons
+the calculator will be limited to evaluating numbers in pairs and returning their
+end value for the next operation. 
---
+++ index.js
@@ -0,0 +1,75 @@
+    //Calculator 
+
+
+//FUNCTIONS
+
+function add(number1, number2) {
+    result = number1 + number2;
+    roundNumber();
+    console.log(result)
+}
+
+function subtract(number1, number2) {
+    result = number1 - number2;
+    roundNumber();
+    console.log(result)
+}
+
+function multiply(number1, number2) {
+    result = number1 * number2;
+    roundNumber();
+    console.log(result)
+}
+
+function divide(number1, number2) {
+    result = number1 / number2;
+    roundNumber();
+    console.log(result)
+}
+
+function calculate(op, number1, number2) {
+    switch (op) {
+        case '+':
+            add(number1, number2);
+            break;
+        case '-':
+            subtract(number1, number2);
+            break;
+        case '*':
+            multiply(number1, number2);
+            break;
+        case '/':
+            divide(number1, number2);
+            break;
+    }
+
+}
+
+//Prevents excessively long decimal numbers
+function roundNumber() {
+    
+    if (!Number.isSafeInteger(result)) {
+        result = result.toFixed(2)
+    }
+    
+}
+
+function display() {
+    
+}
+
+function resetVariables() {
+
+}
+
+function clearScreen() {
+
+}
+
+//GLOBAL VARIABLES
+let num1;
+let num2;
+let operator;
+let result;
+
+calculate ('+', 20, 40);
