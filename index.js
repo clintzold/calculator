7305e049280f4c9c1f105ef90671d98a0dedafc2
Clint Zold <214383878+clintzold@users.noreply.github.com>
2025-07-07 23:36:53 -0600

    Add functionality to all buttons
    Added functions to all buttons so they show up on
    the calculator display.
    
    Created corresponding functions that make
    calculations based on two numbers and a selected
    operator.
    
    Added ability to accumulate results if operators
    rather than equal button is selected.
    
    PROBLEM:
        Has a bug where, after the equal button is
        pressed and results displayed, all following
        calculations assign the value of the first
        operand to the second operand before performing
        the operation.

--- index.js
+++ index.js
@@ -6,50 +6,63 @@
 function add(number1, number2) {
     result = number1 + number2;
     roundNumber();
-    console.log(result)
+    displayScreen.textContent = result;
+    num1 = result; //If operator is repeatedly clicked, value accumulates
+    num2 = null;    //Allows for updating this value in assignValue function
 }
 
 function subtract(number1, number2) {
     result = number1 - number2;
     roundNumber();
-    console.log(result)
+    displayScreen.textContent = result;
+    num1 = result;
+    num2 = null; 
 }
 
 function multiply(number1, number2) {
     result = number1 * number2;
     roundNumber();
-    console.log(result)
+    displayScreen.textContent = result;
+    num1 = result;
+    num2 = null; 
 }
 
 function divide(number1, number2) {
     result = number1 / number2;
     roundNumber();
-    console.log(result)
+    displayScreen.textContent = result;
+    num1 = result;
+    num2 = null; 
 }
 
-function calculate(op, number1, number2) {
-    switch (op) {
+function calculate() {
+    switch (operator) {
         case '+':
-            add(number1, number2);
+            add(num1, num2);
             break;
         case '-':
-            subtract(number1, number2);
+            subtract(num1, num2);
             break;
-        case '*':
-            multiply(number1, number2);
+        case 'x':
+            multiply(num1, num2);
             break;
-        case '/':
-            divide(number1, number2);
+        case '÷':
+            divide(num1, num2);
             break;
     }
 
+    if (equals) {
+        num2 = null;
+        operator = null;
+        equals = false;
+    };
 }
 
-//Prevents excessively long decimal numbers
+//Prevents excessively long decimal number results
 function roundNumber() {
     
     if (!Number.isSafeInteger(result)) {
-        result = result.toFixed(2)
+        result = result.toFixed(3)
     }
     
 }
@@ -58,10 +71,15 @@ function display(string) {
     if (displayScreen.textContent === "0") {
         displayScreen.textContent = null;
         displayScreen.textContent += string;
-    } else if (displayScreen.textContent.length < 15) {
+    } else if (displayScreen.textContent.length < 13 && !operator) {
+        displayScreen.textContent += string;
+    } else if (displayScreen.textContent.length < 13 && operator && !secondEntry) {
+        displayScreen.textContent = string;
+        secondEntry = true;
+    } else if (displayScreen.textContent.length < 13 && operator && secondEntry) {
         displayScreen.textContent += string;
     } else {
-        alert("Cannot exceed 15 characters!");
+        alert("Cannot exceed 13 characters!");
     }
     
 }
@@ -78,7 +96,25 @@ function remove() {
     if (!displayScreen.textContent) displayScreen.textContent = "0";
 }
 
+function assignValue() {
+    const value = displayScreen.textContent;
+    if (!num1) {
+        
+        num1 = parseFloat(value);
+    } else if (num1 && !num2) {
+        console.log('triggered');
+        num2 = parseFloat(value);
+        secondEntry = false;//allows display() to clear screen when chaining equations
+        calculate();
+    };
+};
+
+function addOperator() {
+    displayScreen.textContent = displayScreen.textContent + ` ${operator}`;
+}
+
 function operateButtons(value) {
+
     switch (value) {
         case "clear":
             reset();
@@ -86,19 +122,63 @@ function operateButtons(value) {
         case "delete":
             remove();
             break;
-            
+        case "decimal":
+            if (displayScreen.textContent.includes(".")) break;
+            displayScreen.textContent += ".";
+            break;
+        case "plusMinus":
+            displayScreen.textContent = parseInt(displayScreen.textContent) * -1;
+            break;
+        case "modulo":
+            passValue = parseInt(displayScreen.textContent) / 100;
+            assignValue();
+            displayScreen.textContent += "%";
+            break;
+        case "add":
+            if (displayScreen.textContent.includes("+")) break;
+            assignValue();
+            operator = "+";
+            addOperator();
+            break;
+        case "subtract":
+            if (displayScreen.textContent.includes("-")) break;
+            assignValue();
+            operator = "-";
+            addOperator();
+            break;
+        case "multiply":
+            if (displayScreen.textContent.includes("x")) break;
+            assignValue();
+            operator = "x";
+            addOperator();
+            break;
+        case "divide":
+            if (displayScreen.textContent.includes("÷")) break;
+            assignValue();
+            operator = "÷";
+            addOperator();
+            break;
+        case "equals":
+            equals = true;
+            console.log("clicked");
+            assignValue();
+            break;
+        
     }
 }
 
 //GLOBAL VARIABLES
+let equals;//boolean toggle that resets values during calculation
 let num1;
 let num2;
+let secondEntry;
 let operator;
 let result;
 let displayString;
 const displayScreen = document.getElementById("display");
 const numberButtons = document.querySelectorAll(".button.number");
 const topButtons = document.querySelectorAll(".button.misc");
+const sideButtons = document.querySelectorAll(".button.operator");
 
 //EVENT LISTENERS
 
@@ -110,6 +190,13 @@ numberButtons.forEach(button => {
 })
 
 topButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        const clicked = button.id;
+        operateButtons(clicked);
+    })
+})
+
+sideButtons.forEach(button => {
     button.addEventListener("click", () => {
         const clicked = button.id;
         operateButtons(clicked);
